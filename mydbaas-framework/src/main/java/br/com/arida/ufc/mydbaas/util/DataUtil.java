36573cf622026a37e32ac8edaf8898fe87a62990
araujodavid <david.brasillo@gmail.com>
2013-06-15 01:19:50 -0300

    Adicionar classes utilitárias

---
+++ mydbaas-framework/src/main/java/br/com/arida/ufc/mydbaas/util/DataUtil.java
@@ -0,0 +1,71 @@
+package main.java.br.com.arida.ufc.mydbaas.util;
+
+import java.text.DateFormat;
+import java.text.Format;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public abstract class DataUtil {
+    
+	public static Date convertStringToDate(String data) {   
+		if (data == null || data.equals(""))   
+			return null;              
+	        Date date = null;   
+	        try {   
+	            DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");   
+	            date = (java.util.Date)formatter.parse(data);   
+	        } catch (ParseException e) {               
+	            System.err.println(e.getMessage());
+	        }   
+	        return date;   
+	}
+	  
+	public static String convertDateToString(Date data) {   
+	        if (data == null)   
+	            return null; 	           
+	        String date = null;   
+	        try {   
+	            DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"); 
+	            date = formatter.format(data);   
+	        } catch (Exception e) {               
+	            System.err.println(e.getMessage());
+	        }   
+	        return date;   
+	}
+	
+	public static String convertDateToStringAPI(Date data) {   
+        if (data == null)   
+            return null;            
+        String date = null;   
+        try {   
+            DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
+            date = formatter.format(data);   
+        } catch (Exception e) {               
+            System.err.println(e.getMessage());
+        }   
+        return date;   
+	}
+	
+	public static String convertDateToStringUI(Date data) {   
+        if (data == null)   
+            return null;           
+        String date = null;   
+        try {   
+            DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy"); 
+            date = formatter.format(data);   
+        } catch (Exception e) {               
+            System.err.println(e.getMessage());
+        }   
+        return date;   
+	}
+	
+	public static String formatDate(Date date) {
+		String formatedDate;
+		Format formatter;
+		formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		formatedDate = formatter.format(date);
+		return formatedDate;		
+	}
+}
+
